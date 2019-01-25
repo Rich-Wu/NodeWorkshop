9ddc7b799b7608966a99e76cfc25a15d0457d7ac
Richard <richie1988@gmail.com>
2019-01-25 17:29:21 -0500

    made an express server listen on port:5050

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5050;
+
+app.set('views', './views'); //add a k-v pair to an object
+app.set('view engine', 'ejs');
+
+app.listen(port, () => { console.log(`Express app listening on http://localhost:${port}`);})
