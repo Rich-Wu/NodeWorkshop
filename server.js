9b930da25113d732e0f825eaa953e5d91899d174
Richard <richie1988@gmail.com>
2019-01-25 17:32:09 -0500

    added blank template html files

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const port = 5050;
 app.set('views', './views'); //add a k-v pair to an object
 app.set('view engine', 'ejs');
 
-app.listen(port, () => { console.log(`Express app listening on http://localhost:${port}`);})
+app.listen(port, () => { console.log(`Express app listening on http://localhost:${port}`); })
